87fccf0128766df711e99268ddf4289f78942fe4
Vlad412 <22870742+Vlad412@users.noreply.github.com>
2023-06-12 03:48:34 +0200

    containers

--- index.py
+++ index.py
@@ -1,11 +1,59 @@
+import os
+from dotenv import load_dotenv
 import requests
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import psycopg
+from bs4 import BeautifulSoup
 
-print("Good mroning")
+load_dotenv()  # set environment variables from .env
 
-res = 2 + 2
+database_name = os.getenv("DB_NAME")
+database_user = os.getenv("DB_USER")
+database_password = os.getenv("DB_PASSWORD")
+database_host = os.getenv("DB_HOST")
+target_url = os.getenv("TARGET_URL")  # no slash at the end
 
-print(f"result is {res}")
 
-r = requests.get("https://www.google.com")
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(b"Flemme ok")
 
-print(r.encoding)
+
+def run_server(port):
+    server_address = ("", port)
+    httpd = HTTPServer(server_address, SimpleHTTPRequestHandler)
+    print(f"Server running on port {port}")
+    httpd.serve_forever()
+
+
+run_server(5001)
+
+# Connect to an existing database
+with psycopg.connect(
+    f"host={database_host} port=5432 dbname={database_name} user={database_user} password={database_password}"
+) as conn:
+    # Open a cursor to perform database operations
+    with conn.cursor() as cur:
+        cur.execute("select * from information_schema.tables where table_name=%s", ("things",))
+        val = cur.fetchone()
+        if val == False:
+            cur.execute(
+                """
+                CREATE TABLE things (
+                    id serial PRIMARY KEY,
+                    name text,
+                    power int
+                    data text)
+                """
+            )
+
+        cur.execute("SELECT * FROM things")
+        cur.fetchone()
+
+        for record in cur:
+            print(record)
+
+        conn.commit()
--- readme.md
+++ readme.md
@@ -1,9 +1,19 @@
 # Docker python
 
-[tutorial](https://docs.docker.com/language/python/build-images/)
+[tutorial](https://docs.docker.com/language/python/run-containers/)
+[tutorial](https://docs.docker.com/language/python/run-containers/)
+
+Install poetry
+```sh
+curl -sSL https://install.python-poetry.org | python3 -
+```
 
 ```python
 poetry run python index.py
+```
+
+```sh
+docker compose up -f compose.dev.yaml --build
 ```
 
 ## Dependencies
